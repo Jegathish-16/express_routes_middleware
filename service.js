101c5f267c44b92af60433d7b1ee79b694f05ede
Jegathish-16 <jegathish162004@gmail.com>
2023-05-08 11:46:35 +0530

    Update.Using param method

--- service.js
+++ service.js
@@ -3,19 +3,19 @@ const serviceRouter = express.Router()
 
 const listofservices = [
     {
-        'id' : 1,
+        // 'id' : 1,
         'services' : 'web Development'
     },
     {
-        'id' : 2,
+        // 'id' : 2,
         'services' : 'Node'
     },
     {
-        'id' : 3,
+        // 'id' : 3,
         'services' : 'React'
     },
     {
-        'id' : 4,
+        // 'id' : 4,
         'services' : 'HTML & CSS'
     }
 ]
@@ -25,21 +25,26 @@ serviceRouter.get('/',(request,response) =>{
 })
 
 serviceRouter.get('/:id([0-9]{1})',(request,response) =>{
-    const serId = Number(request.params.id)
-    const ServiceId = listofservices.find((service)=>
-        service.id === serId
-    )
+    // const serId = Number(request.params.id)
+    // const ServiceId = listofservices.find((service)=>
+    //     service.id === serId
+    // )
 
-    if(!ServiceId)
-    {
-        response.send("Service not found")
-    }
-    else
-    {
-        response.send(ServiceId.services)
-    }
-
+    // if(!ServiceId)
+    // {
+    //     response.send("Service not found")
+    // }
+    // else
+    // {
+    //     response.send(ServiceId.services)
+    // }
     // response.send('This is service page')
+    response.send(request.service.services)
+})
+
+serviceRouter.param('id',(request,response,next,id) =>{
+    request.service = listofservices[id-1]
+    next() 
 })
 
 module.exports = serviceRouter;
