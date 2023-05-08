9a4fa4c34a3ad218520b02726df16684bf5c2c8a
Jegathish-16 <jegathish162004@gmail.com>
2023-05-08 11:14:42 +0530

    Add.Service request to service.js

--- service.js
+++ service.js
@@ -1,8 +1,45 @@
 const express = require("express")
 const serviceRouter = express.Router()
 
+const listofservices = [
+    {
+        'id' : 1,
+        'services' : 'web Development'
+    },
+    {
+        'id' : 2,
+        'services' : 'Node'
+    },
+    {
+        'id' : 3,
+        'services' : 'React'
+    },
+    {
+        'id' : 4,
+        'services' : 'HTML & CSS'
+    }
+]
+
 serviceRouter.get('/',(request,response) =>{
     response.send('This is service page')
+})
+
+serviceRouter.get('/:id([0-9]{1})',(request,response) =>{
+    const serId = Number(request.params.id)
+    const ServiceId = listofservices.find((service)=>
+        service.id === serId
+    )
+
+    if(!ServiceId)
+    {
+        response.send("Service not found")
+    }
+    else
+    {
+        response.send(ServiceId.services)
+    }
+
+    // response.send('This is service page')
 })
 
 module.exports = serviceRouter;
