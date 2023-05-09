75498fe7a5cd8b3f8baad2da8607275daa1286c0
Jegathish-16 <jegathish162004@gmail.com>
2023-05-09 08:23:23 +0530

    Update.service.ejs

--- service.js
+++ service.js
@@ -4,19 +4,29 @@ const serviceRouter = express.Router()
 const listofservices = [
     {
         // 'id' : 1,
-        'services' : 'web Development'
+        'services' : 'web Development',
+        'charge' : 2500,
+        'Dur' : 2
     },
     {
         // 'id' : 2,
-        'services' : 'Node'
+        'services' : 'Node',
+        'charge' : 4500,
+        'Dur' : 2
     },
     {
         // 'id' : 3,
-        'services' : 'React'
+        'services' : 'React',
+        'charge' : 5000,
+        'Dur' : 2
+
+
     },
     {
         // 'id' : 4,
-        'services' : 'HTML & CSS'
+        'services' : 'HTML & CSS',
+        'charge' : 3500,
+        'Dur' : 2
     }
 ]
 
@@ -40,12 +50,13 @@ serviceRouter.get('/new',(request,response)=>{
 serviceRouter.post('/',(request,response)=>{
     
     //console.log(request.body.firstname)
-    listofservices.push({services:request.body.servicename})
+    console.log(request.body.servicename,request.body.serviceCost,request.body.serviceduration)
+    listofservices.push({services:request.body.servicename,charge:request.body.serviceCost,Dur:request.body.serviceduration})
     response.redirect(`/service/${listofservices.length}`)
 })
 
-serviceRouter.get('/:id([0-9]{1})',(request,response)=>{
-    response.send(`${request.service.services} with id ${request.params.id} is cost of ${request.service.servicePrice} and it takes ${request.service.serviceTime} to complete`)
+serviceRouter.get('/:id',(request,response)=>{
+    response.send(`${request.service.services} with id =${request.params.id} is cost of =${request.service.charge} and it takes =${request.service.Dur} to complete`)
 })
 
 module.exports = serviceRouter;
